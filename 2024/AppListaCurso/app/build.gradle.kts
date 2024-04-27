77dc07daf5af3d472b6f5aaadf016d5f3a274790
MoacirBarreto <Moacirbarreto.ufrn@gmail.com>
2024-04-27 20:10:51 -0300

    Primeiro Commit

--- 2024/AppListaCurso/app/build.gradle.kts
+++ 2024/AppListaCurso/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "devAndroid.Moacir.applistacurso"
-        minSdk = 26
+        minSdk = 33
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
