f7e1adc8dae714e563d66503cad5a27f0cb30eca
MoacirBarreto <Moacirbarreto.ufrn@gmail.com>
2024-04-30 21:14:21 -0300

    POO 1

---
+++ 2024/AppListaCurso2024/app/src/main/java/devandroid/moacir/applistacurso2024/model/Pessoa.java
@@ -0,0 +1,8 @@
+package devandroid.moacir.applistacurso2024.model;
+
+// construtor da classe
+public class Pessoa {
+
+    public Pessoa(){}
+
+}
--- 2024/AppListaCurso2024/app/src/main/java/devandroid/moacir/applistacurso2024/view/MainActivity.java
+++ 2024/AppListaCurso2024/app/src/main/java/devandroid/moacir/applistacurso2024/view/MainActivity.java
@@ -9,8 +9,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import devandroid.moacir.applistacurso2024.R;
+import devandroid.moacir.applistacurso2024.model.Pessoa;
 
  public class MainActivity extends AppCompatActivity {
+
+     Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,8 @@ import devandroid.moacir.applistacurso2024.R;
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+        pessoa = new Pessoa();
     }
 }
