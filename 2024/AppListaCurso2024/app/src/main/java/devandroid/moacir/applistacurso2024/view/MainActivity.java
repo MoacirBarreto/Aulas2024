f11c33aaf890947cac63781f5c0c5a93074b6a8c
MoacirBarreto <Moacirbarreto.ufrn@gmail.com>
2024-11-29 11:44:07 -0300

    layout com text view

--- 2024/AppListaCurso2024/app/src/main/java/devandroid/moacir/applistacurso2024/view/MainActivity.java
+++ 2024/AppListaCurso2024/app/src/main/java/devandroid/moacir/applistacurso2024/view/MainActivity.java
@@ -1,13 +1,13 @@
 package devandroid.moacir.applistacurso2024.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import devandroid.moacir.applistacurso2024.R;
 import devandroid.moacir.applistacurso2024.model.Pessoa;
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
         // Atribuir conteúdo, dados, valores para o Objeto
@@ -63,11 +62,6 @@ public class MainActivity extends AppCompatActivity {
         editNomeCurso.setText(pessoa.getCursoDesejado());
         editTelefoneContato.setText(pessoa.getTelefoneContato());
 
-//        editPrimeiroNome.setText(outraPessoa.getPrimeiroNome());
-//        editSobreNomeAluno.setText(outraPessoa.getSobreNome());
-//        editNomeCurso.setText(outraPessoa.getCursoDesejado());
-//        editTelefoneContato.setText(outraPessoa.getTelefoneContato());
-
 
         btnLimpar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         btnFinalizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Volte sempre",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Volte sempre", Toast.LENGTH_LONG).show();
                 finish();
             }
         });
@@ -94,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
                 pessoa.setSobreNome(editSobreNomeAluno.getText().toString());
                 pessoa.setCursoDesejado(editNomeCurso.getText().toString());
                 pessoa.setTelefoneContato(editTelefoneContato.getText().toString());
-                Toast.makeText(MainActivity.this,"Salvo "+pessoa.toString(),Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Salvo " + pessoa.toString(), Toast.LENGTH_LONG).show();
             }
         });
 
@@ -116,7 +110,8 @@ public class MainActivity extends AppCompatActivity {
         dadosOutraPessoa += " Telefone de Contato: ";
         dadosOutraPessoa += outraPessoa.getTelefoneContato();
 
-        Log.i("POOAndroid","Objeto pessoa: "+pessoa.toString());
-        Log.i("POOAndroid","Objeto outraPessoa: "+outraPessoa.toString());
+        Log.i("POOAndroid", "Objeto pessoa: " + pessoa.toString());
+        Log.i("POOAndroid", "Objeto outraPessoa: " + outraPessoa.toString());
 
-    }}
+    }
+}
