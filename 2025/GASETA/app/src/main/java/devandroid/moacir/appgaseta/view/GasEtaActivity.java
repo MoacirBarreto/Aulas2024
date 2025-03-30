ff931dacbd3de39e41945bd42dc21dfc296a3d80
MoacirBarreto <Moacirbarreto.ufrn@gmail.com>
2025-03-29 22:45:52 -0300

    Aula 73 - Método de calcular GAS X ETA

--- 2025/GASETA/app/src/main/java/devandroid/moacir/appgaseta/view/GasEtaActivity.java
+++ 2025/GASETA/app/src/main/java/devandroid/moacir/appgaseta/view/GasEtaActivity.java
@@ -1,17 +1,23 @@
 package devandroid.moacir.appgaseta.view;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import devandroid.moacir.appgaseta.R;
+import devandroid.moacir.appgaseta.apoio.UtilGasEta;
 
 public class GasEtaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gaseta);
+
+        Toast.makeText(GasEtaActivity.this,
+                UtilGasEta.calcularMelhorOpcao(5.12, 4.0),
+                Toast.LENGTH_LONG).show();
 
     }
 }
