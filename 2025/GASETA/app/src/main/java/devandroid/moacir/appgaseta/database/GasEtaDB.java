dd5e57d9d40c1e61bbb523f306ab18f1fa2b9c73
MoacirBarreto <Moacirbarreto.ufrn@gmail.com>
2025-03-31 20:09:04 -0300

    Aula 81 - implementação do DB - CRUD

---
+++ 2025/GASETA/app/src/main/java/devandroid/moacir/appgaseta/database/GasEtaDB.java
@@ -0,0 +1,41 @@
+package devandroid.moacir.appgaseta.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class GasEtaDB extends SQLiteOpenHelper {
+    public static final String DB_NAME = "gaseta.db";
+    public static final int DB_VERSION = 1;
+
+    Cursor cursor;
+    SQLiteDatabase db;
+
+
+    public GasEtaDB(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        db = getWritableDatabase();
+    }
+    // métodos para implementar CRUD
+    // Create Retreive Update Delete
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sqlTabelaCombustivel = "CREATE TABLE Combustivel ( " +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "nomeDoCombustivel TEXT, " +
+                "precoDoCombustivel REAL, " +
+                "recomendacao TEXT)";
+        db.execSQL(sqlTabelaCombustivel);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+}
--- 2025/GASETA/app/src/main/java/devandroid/moacir/appgaseta/view/SplashActivity.java
+++ 2025/GASETA/app/src/main/java/devandroid/moacir/appgaseta/view/SplashActivity.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import devandroid.moacir.appgaseta.R;
+import devandroid.moacir.appgaseta.database.GasEtaDB;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -24,6 +25,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                GasEtaDB db = new GasEtaDB(SplashActivity.this);
                 Intent telaPrincipal = new Intent(
                         SplashActivity.this,
                         GasEtaActivity.class);
